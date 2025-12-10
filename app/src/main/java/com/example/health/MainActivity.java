977e2bc9ddf6d64b92375f0cd93f353103d9999d
k10ut4r68 <makarime_1721@outlook.com>
2025-12-10 11:17:13 +0100

    chihaja

--- app/src/main/java/com/example/health/MainActivity.java
+++ app/src/main/java/com/example/health/MainActivity.java
@@ -1,25 +1,42 @@
 package com.example.health;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.health.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     // test &&
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Contient toute la navigation
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        //Gère la navigation
+        NavController navController = navHostFragment.getNavController();
+        //Connecte la bouttom nav an nav controller
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        //affiche / cache la bottom nav selon l'écran
+        /*navController.addOnDestinationChangedListener((controller, destination, arguments) ->{
+            int destId = destination.getId();
+            if(destId == R.id.loginFragment || destId == R.id.registerFragment || destId == R.id.forgotPasswordFragment){
+                binding.bottomNavigationView.setVisibility(View.GONE);
+            } else {
+                binding.bottomNavigationView.setVisibility(View.VISIBLE);
+            }
+        });*/
     }
 }
